6ed5efb8bdb534aa36eb013f941b20aef8068f8b
Salazar Maldonado <550714@CTS.COM>
2016-09-15 17:11:35 -0300

    uploading parcialito1

--- parcialito4/js/controller.js
+++ parcialito4/js/controller.js
@@ -2,10 +2,25 @@ var app = angular.module('starter', []);
 app.controller("productosCtrl", function($scope) {
 	
 $scope.productos = [
-	{ nombre : "producto1", id: 1, precio : 300},
-	{ nombre : "producto2", id: 2, precio : 400},
-	{ nombre : "producto3", id: 3, precio : 359},
-	{ nombre : "producto4", id: 4, precio : 450}
+	{ 
+		nombre : "Carne Mechada", 
+		id: 1, 
+		precio : 30 },
+	{ 
+		nombre : "producto2", 
+		id: 2, 
+		precio : 400
+	},
+	{ 
+		nombre : "producto3", 
+		id: 3, 
+		precio : 359
+	},
+	{ 
+		nombre : "producto4", 
+		id: 4, 
+		precio : 450
+	}
 	];
 	
 	$scope.producto= {
